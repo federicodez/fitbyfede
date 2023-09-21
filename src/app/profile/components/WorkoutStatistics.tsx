066ac3135e9d0c5bbd6b6ac8f3126d5e8a6d9d88
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-09-21 14:40:39 -0400

    added chartjs to profile

--- src/app/profile/components/WorkoutStatistics.tsx
+++ src/app/profile/components/WorkoutStatistics.tsx
@@ -1,25 +1,77 @@
 "use client";
+
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 import { type Workout } from "@/types";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+);
 
 type StatisticsProps = {
   workouts: Workout[];
 };
 
 const Statistics = ({ workouts }: StatisticsProps) => {
-  // const exerciseCount = workouts.reduce((acc, curr) => {
-  //   acc[curr.exercise] = (acc[curr.exercise] || 0) + 1;
-  //   return acc;
-  // }, {});
-  //
-  // const maxed = workouts.reduce((acc, curr, idx) => {
-  //   acc[curr.exercise] = Math.max(...curr.lbs);
-  //   return acc;
-  // }, {});
-  // console.log("statistics: ", maxed);
-
+  const data = {
+    labels: workouts.map((workout) => workout.exercise),
+    datasets: [
+      {
+        data: workouts.map((workout) => workout.lbs[0]),
+      },
+    ],
+  };
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    elements: {
+      line: {
+        tension: 0,
+        borderWidth: 2,
+        borderColor: "rgba(47, 97, 68, 1)",
+        fill: "start",
+        backgroundColor: "rgba(47, 97, 68, 0.3)",
+      },
+      point: {
+        radius: 0,
+        hitRadius: 0,
+      },
+    },
+    scales: {
+      xAxis: {
+        display: false,
+      },
+      yAxis: {
+        display: false,
+      },
+    },
+  };
   return (
-    <div className="profile-card">
-      <h1 className="profile-title">Statistics</h1>
+    <div className="statistics-card">
+      <h1 className="statistics-title">Statistics</h1>
+      <Line data={data} width={200} height={200} options={options} />
     </div>
   );
 };
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,10 @@
 import { Avatar } from "@/components";
 import Navbar from "@/components/navbar/Navbar";
+import Statistics from "./components/WorkoutStatistics";
+import { getWorkouts } from "@/actions";
 
 const Profile = async () => {
+  const workouts = await getWorkouts();
   return (
     <Navbar>
       <section className="profile wrapper container">
@@ -9,6 +12,7 @@ const Profile = async () => {
           <h1 className="profile-title">Profile</h1>
           <Avatar />
         </div>
+        <Statistics workouts={workouts} />
       </section>
     </Navbar>
   );
