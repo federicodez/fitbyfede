6170dd07b651adbcf0b04731be55c764c68d6df4
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-09-24 23:47:33 -0400

    imported and exported workoutform

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 import Avatar from "./Avatar";
+import Button from "./Button";
 import CustomButton from "./CustomButton";
 import EmptyState from "./EmptyState";
 import RemoveBtn from "./RemoveBtn";
+import WorkoutForm from "./WorkoutForm";
 
-export { Avatar, CustomButton, EmptyState, RemoveBtn };
+export { Avatar, Button, CustomButton, EmptyState, RemoveBtn, WorkoutForm };
