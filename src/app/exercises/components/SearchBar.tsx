7ae2bea8ec6aa005e0e9e162196db8907862024a
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-10-12 22:17:58 -0400

    rendering workouts from exercisedb

--- src/app/exercises/components/SearchBar.tsx
+++ src/app/exercises/components/SearchBar.tsx
@@ -4,9 +4,8 @@ import { useState, MouseEvent } from "react";
 import { HiX } from "react-icons/hi";
 import Link from "next/link";
 import { exercises } from "@/constants";
-import { createExercise, createWorkoutSession, createMany } from "@/actions";
+import { getAllWorkouts, getSpecificBodyPart, getCategory } from "@/actions";
 import { useRouter } from "next/navigation";
-import LoadingModel from "@/components/models/LoadingModel";
 import Image from "next/image";
 
 type Data = {
@@ -26,35 +25,9 @@ type SearchBarProps = {
 
 const SearchBar = ({ data }: SearchBarProps) => {
   const [query, setQuery] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [exerciseQueue, setExerciseQueue] = useState<string[]>([]);
   const [details, setDetails] = useState<string | boolean>(false);
 
   const router = useRouter();
-
-  const handleClick = async () => {
-    try {
-      const session = await createWorkoutSession();
-
-      if (session) {
-        await createMany(exerciseQueue, session);
-        setIsLoading(true);
-        router.push(`/finish-workout/${session.id}`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const addToExercises = async (exercise: string) => {
-    if (!exerciseQueue.includes(exercise)) {
-      exerciseQueue.push(exercise);
-    } else {
-      const index = exerciseQueue.indexOf(exercise);
-      exerciseQueue.splice(index, 1);
-    }
-    setExerciseQueue([...exerciseQueue]);
-  };
 
   const filteredExercises =
     query === ""
@@ -68,103 +41,114 @@ const SearchBar = ({ data }: SearchBarProps) => {
 
   return (
     <>
-      {isLoading && <LoadingModel />}
-      <div className="searchbar wrapper container">
-        <div className="flex flex-row justify-between mt-8">
+      <div className="wrapper container">
+        <div
+          className={!details ? `flex flex-row justify-between mt-8` : "hidden"}
+        >
           <button type="button" className="text-[#03045e]" id="create-btn">
             <Link href="/create-workout">New</Link>
           </button>
         </div>
-        <form className="searchbar-form">
-          <input
-            onChange={(e) => setQuery(e.target.value)}
-            type="text"
-            name="query"
-            placeholder="Search"
-            className="searchbar-form__input"
-          />
-        </form>
-        <ul className="container wrapper">
-          {data.map(
-            ({
-              bodyPart,
-              equipment,
-              gifUrl,
-              id,
-              name,
-              target,
-              secondaryMuscles,
-              instructions,
-            }) => (
-              <>
-                <li
-                  key={id}
-                  className={
-                    !details
-                      ? `flex justify-evenly items-center gap-5 m-5 rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
-                      : "hidden"
-                  }
-                  onClick={() => setDetails(id)}
-                >
-                  <Image
-                    id="gif"
-                    src={gifUrl}
-                    height={100}
-                    width={100}
-                    alt="exercise gif"
-                  />
-                  <div className="flex flex-col">
-                    <strong id="name">{name}</strong>
-                    <div className="" id="bodypart">
-                      {bodyPart}
-                    </div>
-                  </div>
-                </li>
-                <li
-                  key={id}
-                  className={
-                    details === id
-                      ? `flex flex-col px-5 rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
-                      : "hidden"
-                  }
-                  onClick={() => setDetails(false)}
-                >
-                  <strong className="text-center" id="name">
+        <h1 className={!details ? `text-center font-bold` : "hidden"}>
+          Exercises
+        </h1>
+        <input
+          onChange={(e) => setQuery(e.target.value)}
+          type="text"
+          name="query"
+          placeholder="Search"
+          className={!details ? `w-full rounded-md bg-white` : "hidden"}
+        />
+        <div
+          className={
+            !details ? `flex justify-evenly items-center my-2` : "hidden"
+          }
+        >
+          <button className="w-fit h-fit rounded-lg bg-gray-50 px-5">
+            Any Body Part
+          </button>
+          <button className="w-fit h-fit rounded-lg bg-gray-50 px-5">
+            Any Category
+          </button>
+        </div>
+        {data.map(
+          ({ bodyPart, gifUrl, id, name, secondaryMuscles, instructions }) => (
+            <>
+              <div
+                key={id}
+                className={
+                  !details
+                    ? `grid grid-cols-6 gap-5 m-5 rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
+                    : "hidden"
+                }
+                onClick={() => setDetails(id)}
+              >
+                <Image
+                  className="col-span-1"
+                  id="gif"
+                  src={gifUrl}
+                  height={100}
+                  width={100}
+                  alt="exercise gif"
+                />
+                <div className="grid grid-rows-2 col-span-5 items-center">
+                  <strong id="name" className="row-span-1">
                     {name}
                   </strong>
-                  <Image
-                    className="flex self-center"
-                    id="gif"
-                    src={gifUrl}
-                    height={400}
-                    width={400}
-                    alt="exercise gif"
-                  />
-                  <h3 className="text-center underline">Instructions</h3>
+                  <div className="row-span-1" id="bodypart">
+                    {bodyPart}
+                  </div>
+                </div>
+              </div>
+              <div
+                key={id}
+                className={
+                  details === id
+                    ? `flex flex-col p-5 my-10 rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
+                    : "hidden"
+                }
+              >
+                <button
+                  className="flex justify-center items-center w-10 h-5 rounded-lg bg-gray-50"
+                  onClick={() => setDetails(false)}
+                >
+                  <HiX />
+                </button>
+                <h3 className="text-center m-2 font-bold" id="name">
+                  {name}
+                </h3>
+                <Image
+                  className="flex self-center rounded-md"
+                  id="gif"
+                  src={gifUrl}
+                  height={400}
+                  width={400}
+                  alt="exercise gif"
+                />
+                <h3 className="text-center m-2 underline font-semibold">
+                  Instructions
+                </h3>
+                <ol className="px-10">
                   {instructions.map((item, Id) => (
-                    <li
-                      className="list-disc list-inside"
-                      key={Id}
-                      id="intructions"
-                    >
+                    <li key={Id} id="intructions" className="list-decimal">
                       {item}
                     </li>
                   ))}
-                  <h3 className="underline m-2">Secondary Mucles</h3>
+                </ol>
+                <h3 className="underline m-2 font-semibold">
+                  Secondary Mucles
+                </h3>
+                <ol className="px-10" type="1">
                   {secondaryMuscles.map((muscle, Id) => (
-                    <li
-                      className="list-disc list-inside"
-                      key={Id}
-                      id="secondary-muscle"
-                    >
+                    <li className="list-decimal" key={Id} id="secondary-muscle">
                       {muscle}
                     </li>
                   ))}
-                </li>
-              </>
-            ),
-          )}
-        </ul>
+                </ol>
+              </div>
+            </>
+          ),
+        )}
       </div>
     </>
   );
