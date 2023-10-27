dd58b30c4c3d973019d37e63eba34a1d2083587d
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-10-27 01:20:45 -0400

    restyled detailed popup

--- src/app/search-workout/components/SearchBar.tsx
+++ src/app/search-workout/components/SearchBar.tsx
@@ -268,14 +268,14 @@ const SearchBar = ({ recentWorkouts }: SearchBarProps) => {
             </button>
           </div>
         </div>
-        <ul className="filtered__list">
+        <ul className="mb-20">
           {recent?.length ? (
             <h3 className={!details ? "most-recent-title" : "hidden"}>
               RECENT
             </h3>
           ) : null}
           {recent?.map(({ bodyPart, gifId, id, name }) => (
-            <div key={id}>
+            <li key={id} className="my-4">
               <div
                 className={
                   !details
@@ -314,7 +314,7 @@ const SearchBar = ({ recentWorkouts }: SearchBarProps) => {
                 </div>
               </div>
 
-              <div
+              <li
                 className={
                   details === id
                     ? `flex flex-col p-5 my-10 rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
@@ -338,110 +338,112 @@ const SearchBar = ({ recentWorkouts }: SearchBarProps) => {
                   width={400}
                   alt="exercise gif"
                 />
-              </div>
-            </div>
+              </li>
+            </li>
           ))}
           <h3 className={!details ? "filtered-title" : "hidden"}>EXERCISES</h3>
-          {filteredExercises?.map(
-            ({ bodyPart, id, name, secondaryMuscles, instructions }) => (
-              <div key={id}>
-                <div
-                  className={
-                    !details
-                      ? `grid grid-cols-4 items-center rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
-                      : "hidden"
-                  }
-                  onClick={() => {
-                    addToExercises(name);
-                  }}
-                >
-                  <Image
-                    className="col-span-1"
-                    id="gif"
-                    src={`https://fitbyfede-db.s3.amazonaws.com/1080/${id}.gif`}
-                    height={100}
-                    width={100}
-                    alt="exercise gif"
-                    priority
-                  />
-                  <div className="col-span-2 flex flex-col items-center mx-2">
-                    <strong id="name" className="w-full">
-                      {name}
-                    </strong>
-                    <div className="w-full" id="bodypart">
-                      {bodyPart}
+          <ul className="mb-20">
+            {filteredExercises?.map(
+              ({ bodyPart, id, name, secondaryMuscles, instructions }) => (
+                <li key={id} className="my-4">
+                  <div
+                    className={
+                      !details
+                        ? `grid grid-cols-4 items-center rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
+                        : "hidden"
+                    }
+                    onClick={() => {
+                      addToExercises(name);
+                    }}
+                  >
+                    <Image
+                      className="col-span-1"
+                      id="gif"
+                      src={`https://fitbyfede-db.s3.amazonaws.com/1080/${id}.gif`}
+                      height={100}
+                      width={100}
+                      alt="exercise gif"
+                      priority
+                    />
+                    <div className="col-span-2 flex flex-col items-center mx-2">
+                      <strong id="name" className="w-full">
+                        {name}
+                      </strong>
+                      <div className="w-full" id="bodypart">
+                        {bodyPart}
+                      </div>
+                    </div>
+                    <div
+                      onClick={() => setDetails(id)}
+                      className="justify-self-end rounded-md bg-white w-fit p-1 mr-5"
+                    >
+                      {exerciseQueue.includes(name) ? (
+                        <AiOutlineCheck />
+                      ) : (
+                        <AiOutlineQuestion />
+                      )}
                     </div>
                   </div>
                   <div
-                    onClick={() => setDetails(id)}
-                    className="justify-self-end rounded-md bg-white w-fit p-1 mr-5"
+                    className={
+                      details === id
+                        ? `flex flex-col p-5 my-2 rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
+                        : "hidden"
+                    }
                   >
-                    {exerciseQueue.includes(name) ? (
-                      <AiOutlineCheck />
-                    ) : (
-                      <AiOutlineQuestion />
-                    )}
+                    <button
+                      className="flex justify-center items-center w-10 h-5 rounded-lg bg-gray-50"
+                      onClick={() => setDetails(false)}
+                    >
+                      <HiX />
+                    </button>
+                    <h3 className="text-center m-2 font-bold" id="name">
+                      {name}
+                    </h3>
+                    <Image
+                      className="flex self-center rounded-md"
+                      id="gif"
+                      // src={`/1080/${id}.gif` as string}
+                      src={`https://fitbyfede-db.s3.amazonaws.com/1080/${id}.gif`}
+                      height={400}
+                      width={400}
+                      alt="exercise gif"
+                      blurDataURL="URL"
+                      placeholder="blur"
+                    />
+                    <h3 className="text-center m-2 underline font-semibold">
+                      Instructions
+                    </h3>
+                    <ol className="px-10">
+                      {instructions.map((item, itemId) => (
+                        <li
+                          key={itemId}
+                          id="intructions"
+                          className="list-decimal"
+                        >
+                          {item}
+                        </li>
+                      ))}
+                    </ol>
+                    <h3 className="underline m-2 font-semibold">
+                      Secondary Mucles
+                    </h3>
+                    <ol className="px-10" type="1">
+                      {secondaryMuscles.map((muscle, muscleId) => (
+                        <li
+                          className="list-decimal"
+                          key={muscleId}
+                          id="secondary-muscle"
+                        >
+                          {muscle}
+                        </li>
+                      ))}
+                    </ol>
                   </div>
-                </div>
-                <div
-                  className={
-                    details === id
-                      ? `flex flex-col p-5 my-10 rounded-md shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(255,255,255),0.3em_0.3em_1em_rgba(0,0,0,0.3)]`
-                      : "hidden"
-                  }
-                >
-                  <button
-                    className="flex justify-center items-center w-10 h-5 rounded-lg bg-gray-50"
-                    onClick={() => setDetails(false)}
-                  >
-                    <HiX />
-                  </button>
-                  <h3 className="text-center m-2 font-bold" id="name">
-                    {name}
-                  </h3>
-                  <Image
-                    className="flex self-center rounded-md"
-                    id="gif"
-                    // src={`/1080/${id}.gif` as string}
-                    src={`https://fitbyfede-db.s3.amazonaws.com/1080/${id}.gif`}
-                    height={400}
-                    width={400}
-                    alt="exercise gif"
-                    blurDataURL="URL"
-                    placeholder="blur"
-                  />
-                  <h3 className="text-center m-2 underline font-semibold">
-                    Instructions
-                  </h3>
-                  <ol className="px-10">
-                    {instructions.map((item, itemId) => (
-                      <li
-                        key={itemId}
-                        id="intructions"
-                        className="list-decimal"
-                      >
-                        {item}
-                      </li>
-                    ))}
-                  </ol>
-                  <h3 className="underline m-2 font-semibold">
-                    Secondary Mucles
-                  </h3>
-                  <ol className="px-10" type="1">
-                    {secondaryMuscles.map((muscle, muscleId) => (
-                      <li
-                        className="list-decimal"
-                        key={muscleId}
-                        id="secondary-muscle"
-                      >
-                        {muscle}
-                      </li>
-                    ))}
-                  </ol>
-                </div>
-              </div>
-            ),
-          )}
+                </li>
+              ),
+            )}
+          </ul>
           <div className={!details ? "mb-10 pb-10" : "hidden"}>
             <Pagination
               currentPage={currentPage}
